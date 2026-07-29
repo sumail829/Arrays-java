7a17f4ff75aa4c0545674e71eb71682129f58583
sumal829 <samirpanjiyar4@gmail.com>
2026-07-29 15:40:24 +0545

    added Input user

---
+++ src/InputUser.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class InputUser{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
