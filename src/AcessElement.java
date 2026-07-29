e0ee2bfb01688006263ce4df3d152a264c6d6e91
sumal829 <samirpanjiyar4@gmail.com>
2026-07-29 14:28:45 +0545

    Accessing elements

---
+++ src/AcessElement.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AcessElement{
+    public static void main(String[] args){
+        int [] s=new int[256];
+        int[] sam={1,2,3,4,5};
+        System.out.println(sam[2]);
+        for(int i=0;i<sam.length;i++){
+            System.out.println(sam[i]);
+        }
+    }
+}
