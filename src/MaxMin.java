d59bcc9f18ec63b57b5a5c5ff7937de3f2f3fee3
sumal829 <samirpanjiyar4@gmail.com>
2026-08-04 13:50:41 +0545

    sorted the element with bubble sort

---
+++ src/MaxMin.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class MaxMin {
+    public static void main(String[] args) {
+        int[] arr={9,1,6,4,10};
+        int sort=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    sort=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=sort;
+                }
+            }
+        }
+        System.out.println("Sorted array is:"+ Arrays.toString(arr));
+    }
+}
